e98c23713f43d08cb998252aec4350656cc1682d
Туркин Артём <artemtu@halykbank.kz>
2018-11-14 16:15:31 +0600

    add ProxyService func

--- proxy.go
+++ proxy.go
@@ -2,7 +2,9 @@ package nprxy
 
 import (
 	"context"
+	"fmt"
 	"net"
+	"net/url"
 )
 
 // DialUpstream func to create conn to upstream service
@@ -19,3 +21,46 @@ var (
 	listenerFactory     = map[string]func(ServiceConfig) (net.Listener, error){}
 	upstreamDialFactory = map[string]func(ServiceConfig) (DialUpstream, error){}
 )
+
+// ProxyService create proxy and forward traffic
+func ProxyService(ctx context.Context, c ServiceConfig) error {
+	u, err := url.Parse(c.Upstream)
+	if err != nil {
+		return fmt.Errorf("failed to parse Upstream: %v", err)
+	}
+
+	// Create proxy with factory
+	pf, ok := proxyFactory[u.Scheme]
+	if !ok {
+		return fmt.Errorf("unsupported upstream scheme %s", u.Scheme)
+	}
+
+	p, err := pf(c)
+	if err != nil {
+		return fmt.Errorf("failed to create proxy: %v", err)
+	}
+
+	// Create listener with factory
+	lf, ok := listenerFactory["plain"]
+	if !ok {
+		return fmt.Errorf("unsupported listener type %s", "TODO")
+	}
+
+	l, err := lf(c)
+	if err != nil {
+		return fmt.Errorf("failed to create listener: %v", err)
+	}
+
+	// Create upstream with factory
+	udf, ok := upstreamDialFactory["plain"]
+	if !ok {
+		return fmt.Errorf("unsupported Upstream dialer type %s", "TODO")
+	}
+
+	ud, err := udf(c)
+	if err != nil {
+		return fmt.Errorf("failed to create upstream dialer: %v", err)
+	}
+
+	return p.Serve(ctx, l, ud)
+}
--- proxy_test.go
+++ proxy_test.go
@@ -1,1 +1,89 @@
 package nprxy_test
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"sync"
+	"testing"
+
+	"github.com/artyomturkin/nprxy"
+)
+
+func testServer() *httptest.Server {
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	}
+	return httptest.NewServer(http.HandlerFunc(handler))
+}
+
+func TestProxy(t *testing.T) {
+	ts := testServer()
+	defer ts.Close()
+
+	service := nprxy.ServiceConfig{
+		Name:     "test",
+		Listen:   "127.0.0.1:59010",
+		Upstream: ts.URL,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	var err error
+	go func() {
+		err = nprxy.ProxyService(ctx, service)
+		wg.Done()
+	}()
+
+	resp, _ := http.Get("http://127.0.0.1:59010/api")
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	cancel()
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Wrong status code: %d, expected 200", resp.StatusCode)
+	}
+	if resp.Header.Get("Content-Type") != "text/html; charset=utf-8" {
+		t.Errorf("wrong Content-Type: %s, expected: text/html; charset=utf-8", resp.Header.Get("Content-Type"))
+	}
+	if string(body) != "<html><body>Hello World!</body></html>" {
+		t.Errorf("Wrong body: %s, expected: <html><body>Hello World!</body></html>", string(body))
+	}
+
+	wg.Wait()
+	if err != http.ErrServerClosed {
+		t.Errorf("Serve failed: %v", err)
+	}
+}
+
+func BenchmarkProxy(b *testing.B) {
+	ts := testServer()
+	defer ts.Close()
+
+	service := nprxy.ServiceConfig{
+		Name:     "test",
+		Listen:   "127.0.0.1:59010",
+		Upstream: ts.URL,
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		nprxy.ProxyService(ctx, service)
+		wg.Done()
+	}()
+
+	for n := 0; n < b.N; n++ {
+		resp, _ := http.Get("http://127.0.0.1:59010/api")
+		ioutil.ReadAll(resp.Body)
+	}
+
+	cancel()
+	wg.Wait()
+}
