f6a5492b282a0693c2ebdf7752c26e9e63ef263b
Туркин Артём <artemtu@halykbank.kz>
2018-11-27 09:40:48 +0600

    update casbin test

--- middleware/casbin_test.go
+++ middleware/casbin_test.go
@@ -9,65 +9,49 @@ import (
 	"github.com/labstack/echo"
 )
 
-func TestCasbinAllowed(t *testing.T) {
-	ce := casbin.NewEnforcer("casbin_model.conf", "casbin_policy.csv")
-
-	e := echo.New()
-
-	req := httptest.NewRequest("GET", "/", nil)
-	res := httptest.NewRecorder()
-
-	c := e.NewContext(req, res)
-	c.Set("system", "alice")
-	c.Set("operation", "data1")
-
-	h := CasbinEnforcer(ce, ValueFromContext("system"), ValueFromContext("operation"))(func(c echo.Context) error {
-		return c.String(http.StatusOK, "test")
-	})
-
-	err := h(c)
-
-	if err != nil {
-		if errObj, ok := err.(*echo.HTTPError); ok {
-			if errObj.Code != 200 {
-				t.Errorf("expected 200, got: %d", errObj.Code)
-			}
-		} else {
-			t.Error(err)
-		}
-	} else if c.Response().Status != 200 {
-		t.Errorf("expected 200, got: %d", c.Response().Status)
+func TestCasbin(t *testing.T) {
+	type testCase struct {
+		name      string
+		system    string
+		operation string
+		result    int
 	}
-}
 
-func TestCasbinForbidden(t *testing.T) {
+	cases := []testCase{
+		testCase{name: "success", system: "alice", operation: "data1", result: 200},
+		testCase{name: "unauth", system: "bob", operation: "data1", result: 403},
+	}
+
 	ce := casbin.NewEnforcer("casbin_model.conf", "casbin_policy.csv")
-
 	e := echo.New()
 
-	req := httptest.NewRequest("GET", "/", nil)
-	res := httptest.NewRecorder()
+	for _, cs := range cases {
+		t.Run(cs.name, func(t *testing.T) {
+			req := httptest.NewRequest("GET", "/", nil)
+			res := httptest.NewRecorder()
 
-	c := e.NewContext(req, res)
-	c.Set("system", "bob")
-	c.Set("operation", "data1")
+			c := e.NewContext(req, res)
+			c.Set("system", cs.system)
+			c.Set("operation", cs.operation)
 
-	h := CasbinEnforcer(ce, ValueFromContext("system"), ValueFromContext("operation"))(func(c echo.Context) error {
-		return c.String(http.StatusOK, "test")
-	})
+			h := CasbinEnforcer(ce, ValueFromContext("system"), ValueFromContext("operation"))(func(c echo.Context) error {
+				return c.String(http.StatusOK, "test")
+			})
 
-	err := h(c)
+			err := h(c)
 
-	if err != nil {
-		if errObj, ok := err.(*echo.HTTPError); ok {
-			if errObj.Code != 403 {
-				t.Errorf("expected 403, got: %d", errObj.Code)
+			if err != nil {
+				if errObj, ok := err.(*echo.HTTPError); ok {
+					if errObj.Code != cs.result {
+						t.Errorf("expected %d, got: %d", cs.result, errObj.Code)
+					}
+				} else {
+					t.Error(err)
+				}
+			} else if c.Response().Status != cs.result {
+				t.Errorf("expected %d, got: %d", cs.result, c.Response().Status)
 			}
-		} else {
-			t.Error(err)
-		}
-	} else if c.Response().Status != 403 {
-		t.Errorf("expected 403, got: %d", c.Response().Status)
+		})
 	}
 }
 
